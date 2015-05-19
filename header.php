b1065e19c54308b4f279d363825a6bc9c828c3e0
miadawinan <miahdawinan@yahoo.com>
2015-05-19 16:59:13 +1200

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!--This is the creation of the header that is applied to ALL pages-->
+<div id="header">
+<h1>SERLER </h1>
+<h3>SERL Evidence Repository</h3>
+</div>
+
+<ul id="menu">
+<!--Link to the home page-->
+ <li><a href="index.php">Practice Search</a></li>
+ <!--Link to the search page-->
+ <li><a href="submitpracticeform.php">Search</a></li>
+ <!--Link to the about page-->
+ <li><a href="about.php">About</a></li> 
+ </ul> 
