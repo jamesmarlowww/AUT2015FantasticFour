d93e9a6fa12c7c8d4dcce0e7626018ff06f82d5b
miadawinan <miahdawinan@yahoo.com>
2015-05-22 16:14:47 +1200

    Create registereduserheader.php

---
+++ SERLER/registereduserheader.php
@@ -0,0 +1,20 @@
+<!--This is the creation of the header that is applied to all pages for registered user-->
+<div id="header">
+<h1>SERLER </h1>
+<h3>SERL Evidence Repository</h3>
+</div>
+
+<ul id="menu">
+	<!--Link to the new search page-->
+	<li><a href="registerednewsearch.php">Practice Search</a></li>
+	<!--Link to the saved searches page-->
+	<li><a href="savedsearches.php">About</a></li> 
+	<!--Link to the submit page-->
+	<li><a href="submitpracticeform.html">Search</a></li>
+	<!--Link to the my practices page-->
+	<li><a href="mypractices.php">About</a></li> 
+	<!--Link to the my account page-->
+	<li><a href="myaccount.php">About</a></li> 
+	<!--Link to the about page-->
+	<li><a href="about.php">About</a></li> 
+ </ul> 
