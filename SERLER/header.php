22393256e9526ef4269d7586c13650a38b5ea23e
Sisky <mackes13@gmail.com>
2015-05-22 15:45:04 -0700

    Tidied everything up
    Compiled it all in to a project file

---
+++ SERLER/header.php
@@ -0,0 +1,15 @@
+<!--This is the creation of the header that is applied to all the pages for unregistered users-->
+<!--atm header and unregistered header are identical. will change once we implement login-->
+<div id="header">
+<h1>SERLER </h1>
+<h3>SERL Evidence Repository</h3>
+</div>
+
+<ul id="menu">
+<!--Link to the home page-->
+ <li><a href="index.php">Practice Search</a></li>
+ <!--Link to the search page-->
+ <li><a href="submitpracticeform.php">Search</a></li>
+ <!--Link to the about page-->
+ <li><a href="about.php">About</a></li> 
+ </ul> 
